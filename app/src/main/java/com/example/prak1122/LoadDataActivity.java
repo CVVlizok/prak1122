f889359b978b467f4ddc0832487e6c3a5be7c29f
CVVlizok <lizaagafonova8@gmail.com>
2024-05-19 19:55:32 +0300

    second app for prak112 (only load)

---
+++ app/src/main/java/com/example/prak1122/LoadDataActivity.java
@@ -0,0 +1,54 @@
+package com.example.prak1122;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_data);
+        // Находим TextView для отображения результата
+        TextView resultView = findViewById(R.id.result);
+
+        // Вызываем метод onClickLoadData сразу после установки ContentView
+        onClickLoadData(resultView);
+    }
+
+    public void onClickLoadData(View view) {
+        // Получаем TextView для отображения результата
+        TextView resultView = findViewById(R.id.result);
+
+        // Выполняем запрос к провайдеру контента для получения данных
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.demo.user.provider/users"), null, null, null, null);
+
+        // Проверяем, есть ли данные в результате запроса
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                // Если есть, строим строку с результатом
+                StringBuilder strBuild = new StringBuilder();
+                int idIndex = cursor.getColumnIndex("id");
+                int nameIndex = cursor.getColumnIndex("name");
+                do {
+                    int id = cursor.getInt(idIndex);
+                    String name = cursor.getString(nameIndex);
+                    strBuild.append("\n" + id + "-" + name);
+                } while (cursor.moveToNext());
+                // Отображаем результат в TextView
+                resultView.setText(strBuild.toString());
+            } else {
+                // Если нет данных, показываем соответствующее сообщение
+                resultView.setText("No Records Found");
+            }
+            cursor.close();
+        } else {
+            Toast.makeText(this, "Cursor is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
